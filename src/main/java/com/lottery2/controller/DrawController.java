b68e0ceb4453206ff4df73da31ce5e5c279ee5d3
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-04-20 16:23:41 +0100

    with responses

--- src/main/java/com/lottery2/controller/DrawController.java
+++ src/main/java/com/lottery2/controller/DrawController.java
@@ -1,10 +1,8 @@
 package com.lottery2.controller;
 
 import java.util.List;
-
-import org.apache.catalina.connector.Response;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.lottery2.DrawNotFoundException;
 import com.lottery2.model.Draw;
 import com.lottery2.repository.DrawRepository;
 
@@ -24,34 +20,34 @@ public class DrawController {
 	@Autowired
 	DrawRepository repository;
 
-  DrawController(DrawRepository repository) {
-    this.repository = repository;
-  }
+	DrawController(DrawRepository repository) {
+		this.repository = repository;
+	}
 
+	@GetMapping("/sorteios")
+	public ResponseEntity<List<Draw>>findAll() {
+		List<Draw> list = repository.findAll();
+		return ResponseEntity.status(200).body(list);
+	}
 
-  @GetMapping("/sorteios")
-  public List<Draw> findAll() {
-	  return repository.findAll();
-  }
- 
-  @GetMapping("/sorteios/{id}")
-  Draw one(@PathVariable Long id) {
-    
-    return repository.findById(id)
-      .orElseThrow(() -> new DrawNotFoundException(id));
-  }
-  
-  /**@GetMapping("/sorteios/{drawNumber}")
-  Draw one(@PathVariable int drawNumber) {
-    
-    return repository.findByDrawNumber(drawNumber)
-      .orElseThrow(() -> new DrawNotFoundException(drawNumber));
-  }*/
-  
-  @PostMapping("/sorteios")
-  public Draw newDraw(@RequestBody Draw newDraw) {
-    return repository.save(newDraw);
-  }
+	@GetMapping("/sorteios/{id}")
+	public ResponseEntity<Optional<Draw>> one(@PathVariable Long id){
+		Optional<Draw> draw = repository.findById(id);
+		return ResponseEntity.status(200).body(draw); 
+	}
+
+	@GetMapping("/sorteios/numero/{drawNumber}")
+	public ResponseEntity<List<Draw>> list(@PathVariable int drawNumber) {
+		List<Draw> list = repository.findByDrawNumber(drawNumber);
+		return ResponseEntity.status(200).body(list);
+	}
+
+	@PostMapping("/sorteios")
+	public ResponseEntity<Draw> newDraw(@RequestBody Draw newDraw) {
+		repository.save(newDraw);
+		return ResponseEntity.status(201).body(newDraw);
+
+	}
 
 
 }
