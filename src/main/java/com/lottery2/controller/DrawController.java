8495ed52fab5875155b8223360d5462a83279aa9
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-04-21 12:39:19 +0100

    with put method

--- src/main/java/com/lottery2/controller/DrawController.java
+++ src/main/java/com/lottery2/controller/DrawController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,16 @@ public class DrawController {
 
 	}
 
-
+	@DeleteMapping("/sorteios/{id}")
+	public ResponseEntity<Draw> delete(@PathVariable Long id){
+		repository.deleteById(id);
+		return ResponseEntity.noContent().build(); 
+	}
+	
+	@PutMapping("/sorteios/{id}")
+	public ResponseEntity<Draw> replace(@RequestBody Draw draw, @PathVariable Long id){
+		repository.deleteById(id);
+		repository.save(draw);
+		return ResponseEntity.status(201).body(draw);
+	}
 }
