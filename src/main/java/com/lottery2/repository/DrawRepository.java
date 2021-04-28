400bf4261f4bae3fc9efe5a095fa87b07cbcdcda
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-04-28 16:04:52 +0100

    with frequency

--- src/main/java/com/lottery2/repository/DrawRepository.java
+++ src/main/java/com/lottery2/repository/DrawRepository.java
@@ -1,9 +1,13 @@
 package com.lottery2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lottery2.model.Draw;
 
 public interface DrawRepository extends JpaRepository<Draw, Long> {
 
+	List<Draw> findByDrawNumber(int drawNumber);
+	
 }
